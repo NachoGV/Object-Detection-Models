e51b4f42c2c44658475710fa461b0d438c55b388
NachoGV <ignacio.gvalverde@gmail.com>
2024-01-03 15:57:43 +0100

    Added data formatting notebook file
    To be done for the moment:
    * Normalice the annotations for yolo in the data2yolo function (not started)
    * Own version of the YOLO training (started)
    * data-preparation.ipynb file to prepare the data for the training without using the command line (not started)
    * Test other models such as FasterRCNN and SSD (not started)
    * Rewrite the report to make it my own (not started)
    * Add missing links to the README.md file (not started, requieres the previous points to be done)

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,4 @@
 
-# PATHS
-
 # Categorized ExDark dataset
 CAT_IMAGES = '../data/ExDark/'
 CAT_ANNOTATIONS = '../data/ExDark_Annno'
@@ -14,16 +12,34 @@ TRAIN_COCO = '../data/ExDark_COCO/train_set.json'
 VAL_COCO = '../data/ExDark_COCO/val_set.json'
 TEST_COCO = '../data/ExDark_COCO/test_set.json'
 
-# YOLO Format dataset
+# YOLO Format dataset (root)
+YOLO_IMGS = '../data/ExDark_YOLO/images'
+YOLO_ANOS = '../data/ExDark_YOLO/annotations'
+YOLO_LABELS = '../data/ExDark_YOLO/labels'
+YOLO_YAML = '../data/ExDark_YOLO/yolo.yaml'
+
+# YOLO Format dataset (train, val, test)
 TRAIN_YOLO_IMGS = '../data/ExDark_YOLO/images/train'
 TRAIN_YOLO_ANOS = '../data/ExDark_YOLO/annotations/train'
 VAL_YOLO_IMGS = '../data/ExDark_YOLO/images/val'
 VAL_YOLO_ANOS = '../data/ExDark_YOLO/annotations/val'
 TEST_YOLO_IMGS = '../data/ExDark_YOLO/images/test'
 TEST_YOLO_ANOS = '../data/ExDark_YOLO/annotations/test'
-YOLO_LABELS = '../data/ExDark_YOLO/labels'
-YOLO_YAML = '../data/ExDark_YOLO/yolo.yaml'
 
 # Outputs
 DETR_OUT = '../outputs/transformer'
 YOLO_OUT = '../outputs/yolo'
+
+# Labels
+LABELS = {'Bicycle': 0, 
+          'Boat': 1, 
+          'Bottle': 2, 
+          'Bus': 3, 
+          'Car': 4, 
+          'Cat': 5, 
+          'Chair': 6, 
+          'Cup': 7, 
+          'Dog': 8, 
+          'Motorbike': 9, 
+          'People': 10, 
+          'Table': 11}
--- utils/format_datasets.py
+++ utils/format_datasets.py
@@ -4,9 +4,162 @@ import json
 import torch
 import shutil
 from tqdm import tqdm
-from constants import *
-from datasets_generators import CocoDetection
+from data_generator import CocoDetection
 from transformers import DetrImageProcessor, DetrForObjectDetection
+from constants import CAT_IMAGES, CAT_ANNOTATIONS, ALL_IMAGES, ALL_ANNOTATIONS, TRAIN_COCO, VAL_COCO, TEST_COCO, DETR_OUT, YOLO_IMGS, YOLO_ANOS, YOLO_LABELS, YOLO_YAML, LABELS, TRAIN_YOLO_IMGS, TRAIN_YOLO_ANOS, VAL_YOLO_IMGS, VAL_YOLO_ANOS, TEST_YOLO_IMGS, TEST_YOLO_ANOS
+
+def data2all():
+
+    # Create Directories
+    os.makedirs(ALL_IMAGES, exist_ok=True)
+    os.makedirs(ALL_ANNOTATIONS, exist_ok=True)
+
+    # Copy images to the directory
+    print('Copying images...')
+    for label_dir in tqdm(os.listdir(CAT_IMAGES)):
+        for img in os.listdir(f'{CAT_IMAGES}/{label_dir}'):
+            shutil.copy(f'{CAT_IMAGES}/{label_dir}/{img}', f'{ALL_IMAGES}/{img}')
+
+    # Copy annotations to the directory
+    print('Copying annotations...')
+    for label_dir in tqdm(os.listdir(CAT_ANNOTATIONS)):
+        for anno in os.listdir(f'{CAT_ANNOTATIONS}/{label_dir}'):
+            shutil.copy(f'{CAT_ANNOTATIONS}/{label_dir}/{anno}', f'{ALL_ANNOTATIONS}/{anno}')
+
+    print('\nNumber of images: ', len(os.listdir(ALL_IMAGES)))
+    print('Number of annotations: ', len(os.listdir(ALL_ANNOTATIONS)))
+
+
+# -----------------------------------------------------------------------------------------------
+    
+def img2jpg():
+
+    print("Converting all images to JPG...")
+
+    print(ALL_IMAGES)
+    for img in tqdm(os.listdir(ALL_IMAGES)):
+        name = img.split(".")[0]
+        format = img.split(".")[1]
+        if format != "jpg":
+            os.rename(f'{ALL_IMAGES}/{img}', f'{ALL_IMAGES}/{name}.jpg')
+
+    print(CAT_IMAGES)
+    for label_dir in tqdm(os.listdir(CAT_IMAGES)):
+        for img in os.listdir(f'{CAT_IMAGES}/{label_dir}'):
+            name = img.split(".")[0]
+            format = img.split(".")[1]
+            if format != "jpg":
+                os.rename(f'{CAT_IMAGES}/{label_dir}/{img}', f'{CAT_IMAGES}/{label_dir}/{name}.jpg')
+
+    print(ALL_ANNOTATIONS)
+    for anno in tqdm(os.listdir(ALL_ANNOTATIONS)):
+        name = anno.split(".")[0]
+        format = anno.split(".")[1]
+        if format != "jpg":
+            os.rename(f'{ALL_ANNOTATIONS}/{anno}', f'{ALL_ANNOTATIONS}/{name}.jpg.txt')
+
+    print(CAT_ANNOTATIONS)
+    for label_dir in tqdm(os.listdir(CAT_ANNOTATIONS)):
+        for anno in os.listdir(f'{CAT_ANNOTATIONS}/{label_dir}'):
+            name = anno.split(".")[0]
+            format = anno.split(".")[1]
+            if format != "jpg":
+                os.rename(f'{CAT_ANNOTATIONS}/{label_dir}/{anno}', f'{CAT_ANNOTATIONS}/{label_dir}/{name}.jpg.txt')
+
+# -----------------------------------------------------------------------------------------------
+                
+def data2YOLO():
+
+    # Define Splits
+    print('Splitting...')
+
+    train_list = []
+    val_list = []
+    test_list = []
+    for subf in os.listdir(CAT_IMAGES):
+        subdir = os.path.join(CAT_IMAGES, subf)
+        imgs = os.listdir(subdir)
+        train_list.extend(imgs[:250])
+        val_list.extend(imgs[250:400])
+        test_list.extend(imgs[400:])
+
+    # Create Directories
+    print('Creating directories...')
+
+    os.makedirs(YOLO_IMGS, exist_ok=True)
+    os.makedirs(YOLO_ANOS, exist_ok=True)
+    os.makedirs(YOLO_LABELS, exist_ok=True)
+
+    os.makedirs(TRAIN_YOLO_IMGS, exist_ok=True)
+    os.makedirs(TRAIN_YOLO_ANOS, exist_ok=True)
+
+    os.makedirs(VAL_YOLO_IMGS, exist_ok=True)
+    os.makedirs(VAL_YOLO_ANOS, exist_ok=True)
+
+    os.makedirs(TEST_YOLO_IMGS, exist_ok=True)
+    os.makedirs(TEST_YOLO_ANOS, exist_ok=True)
+    
+    # Copy images to the directory
+    print('Copying images...')
+    for img in tqdm(os.listdir(ALL_IMAGES)):
+        
+        if img in train_list:
+            shutil.copy(f'{ALL_IMAGES}/{img}', f'{TRAIN_YOLO_IMGS}/{img}')
+        elif img in val_list:
+            shutil.copy(f'{ALL_IMAGES}/{img}', f'{VAL_YOLO_IMGS}/{img}')
+        elif img in test_list:
+            shutil.copy(f'{ALL_IMAGES}/{img}', f'{TEST_YOLO_IMGS}/{img}')
+
+    # Copy annotations to the directory
+    print('Formatting annotations...')
+    for anno in tqdm(os.listdir(ALL_ANNOTATIONS)):
+        
+        img = anno.split('.')[0] + '.jpg'
+
+        if img in train_list:
+            with open(f'{ALL_ANNOTATIONS}/{anno}', 'r') as f:
+                lines = f.readlines()[1:]
+                with open(f'{TRAIN_YOLO_ANOS}/{anno}', 'w') as f:
+                    for l in lines:
+                        f.write(f'{LABELS[l.split(" ")[0]]} {l.split(" ")[1]} {l.split(" ")[2]} {l.split(" ")[3]} {l.split(" ")[4]}\n')
+
+        elif img in val_list:
+            with open(f'{ALL_ANNOTATIONS}/{anno}', 'r') as f:
+                lines = f.readlines()[1:]
+                with open(f'{VAL_YOLO_ANOS}/{anno}', 'w') as f:
+                    for l in lines:
+                        f.write(f'{LABELS[l.split(" ")[0]]} {l.split(" ")[1]} {l.split(" ")[2]} {l.split(" ")[3]} {l.split(" ")[4]}\n')
+
+        elif img in test_list:
+            with open(f'{ALL_ANNOTATIONS}/{anno}', 'r') as f:
+                lines = f.readlines()[1:]
+                with open(f'{TEST_YOLO_ANOS}/{anno}', 'w') as f:
+                    for l in lines:
+                        f.write(f'{LABELS[l.split(" ")[0]]} {l.split(" ")[1]} {l.split(" ")[2]} {l.split(" ")[3]} {l.split(" ")[4]}\n')
+
+    # Create yaml file
+    print('Creating yaml file...')
+    with open(YOLO_YAML, 'w') as f:
+        f.write(f'path: ../ExDark_YOLO\n')
+        f.write(f'train: images/train\n')
+        f.write(f'val: images/val\n')
+        f.write(f'test: images/test\n')
+        f.write(f'names:\n')
+        for k, v in LABELS.items():
+            f.write(f'  {v}: {k}\n')
+
+    # Summary
+    print('\nNumber of images: ', len(os.listdir(ALL_IMAGES)))
+    print('Number of annotations: ', len(os.listdir(ALL_ANNOTATIONS)))
+    print('\nNumber of train images: ', len(os.listdir(TRAIN_YOLO_IMGS)))
+    print('Number of train annotations: ', len(os.listdir(TRAIN_YOLO_ANOS)))
+    print('\nNumber of val images: ', len(os.listdir(VAL_YOLO_IMGS)))
+    print('Number of val annotations: ', len(os.listdir(VAL_YOLO_ANOS)))
+    print('\nNumber of test images: ', len(os.listdir(TEST_YOLO_IMGS)))
+    print('Number of test annotations: ', len(os.listdir(TEST_YOLO_ANOS)))
+
+
+# -----------------------------------------------------------------------------------------------
 
 def data2COCO():
     # Initialize COCO json format
@@ -52,11 +205,6 @@ def data2COCO():
         train_list.extend(imgs[:250])
         val_list.extend(imgs[250:400])
         test_list.extend(imgs[400:])
-
-    # Print values
-    print('  Length of train set: ', len(train_list))
-    print('  Length of val set: ', len(val_list))
-    print('  Length of test set: ', len(test_list))
 
     print('Generating Training Set Json...')
 
@@ -133,58 +281,16 @@ def data2COCO():
     print('Saving...')
 
     # Save Files
-    json.dump(train_json, open(TRAIN_COCO), 'w')
+    json.dump(train_json, open(TRAIN_COCO, 'w'))
     json.dump(val_json, open(VAL_COCO, 'w'))
     json.dump(test_json, open(TEST_COCO, 'w'))
 
-    print('Done')
-
-# -----------------------------------------------------------------------------------------------
-    
-def img2jpg():
-    all_imgs = "../ExDark_All/Images"
-    all_annos = "../ExDark_All/Annotations"
-    imgs = "../ExDark/ExDark"
-    annos = "../ExDark_Annno/ExDark_Annno"
-
-    print("Converting all images to JPG...")
-
-    print(all_imgs)
-    for img in tqdm(os.listdir(all_imgs)):
-        name = img.split(".")[0]
-        format = img.split(".")[1]
-        if format != "jpg":
-            os.rename(f'{all_imgs}/{img}', f'{all_imgs}/{name}.jpg')
-
-    print(imgs)
-    for label_dir in tqdm(os.listdir(imgs)):
-        for img in tqdm(os.listdir(f'{imgs}/{label_dir}')):
-            name = img.split(".")[0]
-            format = img.split(".")[1]
-            if format != "jpg":
-                os.rename(f'{imgs}/{label_dir}/{img}', f'{imgs}/{label_dir}/{name}.jpg')
-
-    print(all_annos)
-    for anno in tqdm(os.listdir(all_annos)):
-        name = anno.split(".")[0]
-        format = anno.split(".")[1]
-        if format != "jpg":
-            os.rename(f'{all_annos}/{anno}', f'{all_annos}/{name}.jpg.txt')
-
-    print(annos)
-    for label_dir in tqdm(os.listdir(annos)):
-        for anno in tqdm(os.listdir(f'{annos}/{label_dir}')):
-            name = anno.split(".")[0]
-            format = anno.split(".")[1]
-            if format != "jpg":
-                os.rename(f'{annos}/{label_dir}/{anno}', f'{annos}/{label_dir}/{name}.jpg.txt')
+    print('Done!')
 
 # -----------------------------------------------------------------------------------------------
 
-def dert_results2COCO():
+def dert_results2COCO(enhancement):
     name = None
-    enhancement = None
-    inp = input("Enter enhancement:\n  1. Clahe\n  2. Color Balance Adjustement\n  3. Histogram Equialization\n  4. No Enhancement\n>")
     if inp == "1":
         name = "clahe/output"
         enhancement = "clahe"
@@ -197,21 +303,16 @@ def dert_results2COCO():
     elif inp == "4":
         name = "raw/output"
         enhancement = 'raw'
-    else:
-        print("Invalid input")
-        return
 
     # Device
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # Paths
-    imgs_path = "../ExDark_All/Images"
-    test_path = "../ExDark_COCO/test_set.json"
-    model_path = f"../Models/Transformer/lightning_logs/{name}"
+    model_path = DETR_OUT + '/' + name
 
     # Initialization
     image_processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
-    test_set = CocoDetection(image_directory_path=imgs_path,  annotation_file_path=test_path, image_processor=image_processor, enhancement=enhancement)
+    test_set = CocoDetection(image_directory_path=ALL_IMAGES,  annotation_file_path=TEST_COCO, image_processor=image_processor, enhancement=enhancement)
     model = DetrForObjectDetection.from_pretrained(model_path).to(device)
     results = {"annotations": []}
     
@@ -223,7 +324,7 @@ def dert_results2COCO():
         _, anno = test_set[i]
         file_name = test_set.coco.loadImgs(i)[0]['file_name']
         original_size = anno['orig_size']
-        cv_image = cv2.imread(os.path.join(imgs_path, file_name))
+        cv_image = cv2.imread(os.path.join(ALL_IMAGES, file_name))
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
 
         # Prediction
@@ -254,91 +355,6 @@ def dert_results2COCO():
         json.dump(results, fp)
 
 # -----------------------------------------------------------------------------------------------
-        
-def data2YOLO():
-
-    cat_dir = '../ExDark/ExDark'
-    images = '../ExDark_All/Images'
-    annos = '../ExDark_All/Annotations'
-    labels = {'Bicycle': 0, 
-              'Boat': 1, 
-              'Bottle': 2, 
-              'Bus': 3, 
-              'Car': 4, 
-              'Cat': 5, 
-              'Chair': 6, 
-              'Cup': 7, 
-              'Dog': 8, 
-              'Motorbike': 9, 
-              'People': 10, 
-              'Table': 11}
-
-    # Define Splits
-    print('Splitting...')
-
-    train_list = []
-    val_list = []
-    test_list = []
-    for subf in os.listdir(cat_dir):
-        subdir = os.path.join(cat_dir, subf)
-        imgs = os.listdir(subdir)
-        train_list.extend(imgs[:250])
-        val_list.extend(imgs[250:400])
-        test_list.extend(imgs[400:])
-
-    # Create Directories
-    print('Creating directories...')
-
-    os.makedirs('../ExDark_YOLO/images', exist_ok=True)
-    os.makedirs('../ExDark_YOLO/annotations', exist_ok=True)
-
-    os.makedirs('../ExDark_YOLO/images/train', exist_ok=True)
-    os.makedirs('../ExDark_YOLO/images/val', exist_ok=True)
-    os.makedirs('../ExDark_YOLO/images/test', exist_ok=True)
-
-    os.makedirs('../ExDark_YOLO/annotations/train', exist_ok=True)
-    os.makedirs('../ExDark_YOLO/annotations/val', exist_ok=True)
-    os.makedirs('../ExDark_YOLO/annotations/test', exist_ok=True)
-    
-    # Copy images to the directory
-    print('Copying images...')
-    for img in tqdm(os.listdir('../ExDark_All/Images')):
-        
-        if img in train_list:
-            shutil.copy(f'{images}/{img}', f'../ExDark_YOLO/images/train/{img}')
-        elif img in val_list:
-            shutil.copy(f'{images}/{img}', f'../ExDark_YOLO/images/val/{img}')
-        elif img in test_list:
-            shutil.copy(f'{images}/{img}', f'../ExDark_YOLO/images/test/{img}')
-
-    # Copy annotations to the directory
-    print('Formatting annotations...')
-    for anno in tqdm(os.listdir('../ExDark_All/Annotations')):
-        
-        img = anno.split('.')[0] + '.jpg'
-
-        if img in train_list:
-            with open(f'{annos}/{anno}', 'r') as f:
-                lines = f.readlines()[1:]
-                with open(f'../ExDark_YOLO/annotations/train/{anno}', 'w') as f:
-                    for l in lines:
-                        f.write(f'{labels[l.split(" ")[0]]} {l.split(" ")[1]} {l.split(" ")[2]} {l.split(" ")[3]} {l.split(" ")[4]}\n')
-
-        elif img in val_list:
-            with open(f'{annos}/{anno}', 'r') as f:
-                lines = f.readlines()[1:]
-                with open(f'../ExDark_YOLO/annotations/val/{anno}', 'w') as f:
-                    for l in lines:
-                        f.write(f'{labels[l.split(" ")[0]]} {l.split(" ")[1]} {l.split(" ")[2]} {l.split(" ")[3]} {l.split(" ")[4]}\n')
-
-        elif img in test_list:
-            with open(f'{annos}/{anno}', 'r') as f:
-                lines = f.readlines()[1:]
-                with open(f'../ExDark_YOLO/annotations/test/{anno}', 'w') as f:
-                    for l in lines:
-                        f.write(f'{labels[l.split(" ")[0]]} {l.split(" ")[1]} {l.split(" ")[2]} {l.split(" ")[3]} {l.split(" ")[4]}\n')
-
-# -----------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     function = input("Select function:\n  1 - img2jpg\n  2 - data2YOLO\n  3 - data2COCO\n  4 - dert_results2COCO\n>")
@@ -349,6 +365,16 @@ if __name__ == "__main__":
     elif function == "3":
         data2COCO()
     elif function == "4":
-        dert_results2COCO()
+        inp = input("Enter enhancement:\n  1. Clahe\n  2. Color Balance Adjustement\n  3. Histogram Equialization\n  4. No Enhancement\n>")
+        if inp == "1":
+            dert_results2COCO('1')
+        elif inp == "2":
+            dert_results2COCO('2')
+        elif inp == "3":
+            dert_results2COCO('3')
+        elif inp == "4":
+            dert_results2COCO('4')
+        else:
+            print("Invalid enhancement")
     else:
         print("Invalid function")
